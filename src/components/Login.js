b9c3d2ee252d2326beba325fea14a083bf15cc0d
Balemarthy, Bhaskar Gnyana <gnyana@gatech.edu>
2021-10-23 15:23:19 -0400

    Second Commit

---
+++ src/components/Login.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const appStyle = {
+	height: '250px',
+  	display: 'flex'
+};
+
+const formStyle = {
+    margin: 'auto',
+    padding: '10px',
+    border: '1px solid #c9c9c9',
+    borderRadius: '5px',
+    background: '#aeb2b6',
+    width: '220px',
+  	display: 'block'
+};
+
+const labelStyle = {
+    margin: '10px 0 5px 0',
+    fontFamily: 'Arial, Helvetica, sans-serif',
+    fontSize: '15px',
+    fontWeight: '300'
+};
+
+const inputStyle = {
+    margin: '5px 0 10px 0',
+    padding: '5px', 
+    border: '1px solid #bfbfbf',
+    borderRadius: '3px',
+    boxSizing: 'border-box',
+    width: '100%'
+};
+
+const submitStyle = {
+    margin: '10px 0 0 0',
+    padding: '7px 10px',
+    border: '1px solid #efffff',
+    borderRadius: '3px',
+    background: '#2c8671',
+    width: '100%', 
+    fontSize: '15px',
+    color: 'black',
+    display: 'block'
+};
+
+const Field = React.forwardRef(({label, type}, ref) => {
+    return (
+      <div>
+        <label style={labelStyle} >{label}</label>
+        <input ref={ref} type={type} style={inputStyle} />
+      </div>
+    );
+});
+
+const Form = ({onSubmit}) => {
+    const usernameRef = React.useRef();
+    const passwordRef = React.useRef();
+    const handleSubmit = e => {
+        e.preventDefault();
+        const data = {
+            username: usernameRef.current.value,
+            password: passwordRef.current.value
+        };
+        onSubmit(data);
+    };
+    return (
+      <form style={formStyle} onSubmit={handleSubmit} >
+        <Field ref={usernameRef} label="Username:" type="text" />
+        <Field ref={passwordRef} label="Password:" type="password" />
+        <div>
+          <button style={submitStyle} type="submit">Submit</button>
+        </div>
+      </form>
+    );
+};
+
+// Usage example:
+
+const Login = () => {
+    const handleSubmit = data => {
+        const json = JSON.stringify(data, null, 4);
+        console.clear();
+        console.log(json);
+        console.log('Submitted: ' + {data});
+    };
+    return (
+      <div style={appStyle}>
+        <Form onSubmit={handleSubmit} />
+      </div>
+    );
+};
+
+const root = document.querySelector('#root');
+
+
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {theme, CSSReset, ChakraProvider} from "@chakra-ui/react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const breakpoints = ["360px", "768px", "1024px", "1440px"];
+breakpoints.sm = breakpoints[0];
+breakpoints.md = breakpoints[1];
+breakpoints.lg = breakpoints[2];
+breakpoints.xl = breakpoints[3];
+
+const newTheme = {
+  ...theme,
+  breakpoints
+};
+
+
+function Index() {
+  return(
+    <ChakraProvider theme = {newTheme}>
+      <CSSReset></CSSReset>
+      <App />
+    </ChakraProvider>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Index/>, rootElement);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
